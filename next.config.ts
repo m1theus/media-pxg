669e2335963d0e5fb62c7ceefe0d6a5fc5fbf9dd
Matheus Martins <mail@mmartins.dev>
2024-12-08 18:10:07 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,18 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/*/**",
       },
+      {
+        protocol: "https",
+        hostname: "vercel.app",
+        port: "",
+        pathname: "/*/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media-pxg.vercel.app",
+        port: "",
+        pathname: "/*/**",
+      },
     ],
   },
   output: "standalone",
